59d263fa5d46be0d0855b89a5cb191f91e72f491
MedishettyIshwarya <medishettyishwarya114@gmail.com>
2025-10-04 16:21:02 +0530

    ishu

--- hi.py
+++ hi.py
@@ -3,3 +3,21 @@ print("Hello, Ishwarya! Your VS Code is working perfectly 🚀")
 def reverse_string(n):
     return n[::-1]
 print(reverse_string("ishu"))
+#palindrome
+def  palindrome(n):
+    return n==n[::-1]
+print(palindrome("madam"))
+
+def palindrome(n):
+    if n%10==0:
+        return False
+    rev=0
+    temp=n
+    while temp>0:
+        rev=rev*10+temp%10
+        temp//=10
+        return rev==n       
+print(palindrome(121))
+        
+    
+
