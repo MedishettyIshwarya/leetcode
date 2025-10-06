120bda4dd7cbbe717ea6d1c3c9525cd85d618cb8
MedishettyIshwarya <medishettyishwarya114@gmail.com>
2025-10-06 20:39:17 +0530

    ishu

--- hi.py
+++ hi.py
@@ -84,3 +84,12 @@ def fibonacci(n):
         a,b=b,a+b
     return fib_series
 print(fibonacci(10))
+#Prime number
+def is_prime(n):
+    if n%2==0 and n>2:
+        return False
+    for i in range(3,int(n**0.5)+1,2):
+        if n%i==0:
+            return False        
+    return True
+print(is_prime(29))
