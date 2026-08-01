2059dfd7828e13cd4cf3352836f4cad148716665
Medishetty Ishwarya <medishettyishwarya114@gmail.com>
2026-08-02 00:18:31 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/7. Reverse Integer/Solution.java
+++ LeetCode/Java/Medium/7. Reverse Integer/Solution.java
@@ -3,6 +3,7 @@ class Solution {
         int rev = 0;
         while(x!=0){
             int  digit= x%10;
+            //
              if (rev > Integer.MAX_VALUE / 10 || 
                 (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
                 return 0;
