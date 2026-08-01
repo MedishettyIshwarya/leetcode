3149a9f45a81d97a11df7160981241061f496571
Medishetty Ishwarya <medishettyishwarya114@gmail.com>
2026-08-02 00:17:22 +0530

    Add Java Solution to Patterns/Math

--- LeetCode/Patterns/Math/7. Reverse Integer/Solution.java
+++ LeetCode/Patterns/Math/7. Reverse Integer/Solution.java
@@ -3,6 +3,10 @@ class Solution {
         int rev = 0;
         while(x!=0){
             int  digit= x%10;
+             if (rev > Integer.MAX_VALUE / 10 || 
+                (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
             rev = rev*10+ digit;
             x=x/10;
 
